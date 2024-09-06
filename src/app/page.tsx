9da6171434eeb777945ae9f1c3f89cb04a2af3d7
Pedro Craveiro <pecraveiro1@gmail.com>
2024-09-06 01:48:28 -0400

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -200,13 +200,14 @@ export default function Page() {
           <BlurFade delay={BLUR_FADE_DELAY * 16}>
             <div className="space-y-3">
               <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
-                Contact
+                Contato
               </div>
               <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                Get in Touch
+                Vamos construir juntos e tornar suas ideias em realidade.
               </h2>
               <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                 Want to chat? Just shoot me a dm{" "}
+                
                 <Link
                   href={DATA.contact.social.X.url}
                   className="text-blue-500 hover:underline"
